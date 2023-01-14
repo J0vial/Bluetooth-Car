d75e7c65b99085b2a8331d985d9b05a63e1bf4b4
Md Shakibul Alam <70951378+J0vial@users.noreply.github.com>
2023-01-14 21:30:51 +0600

    Add files via upload

---
+++ bluetooth_car.ino
@@ -0,0 +1,105 @@
+int m1a = 9;
+int m1b = 10;
+int m2a = 11;
+int m2b = 12;
+int horn  = 8;
+
+char val;
+
+void setup()
+{
+  pinMode(m1a, OUTPUT);  // Digital pin 9 set as output Pin
+  pinMode(m1b, OUTPUT);  // Digital pin 10 set as output Pin
+  pinMode(m2a, OUTPUT);  // Digital pin 11 set as output Pin
+  pinMode(m2b, OUTPUT);  // Digital pin 12 set as output Pin
+  pinMode(horn, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  while (Serial.available() > 0)
+  {
+    val = Serial.read();
+    //if(val == 'V'){
+    Serial.println(val);
+  }
+
+  if( val == 'F') // Forward
+    {
+      digitalWrite(m1a, HIGH);
+      digitalWrite(m1b, LOW);
+      digitalWrite(m2a, HIGH);
+      digitalWrite(m2b, LOW);  
+    }
+    else if(val == 'B') // Backward
+    {
+      digitalWrite(m1a, LOW);
+      digitalWrite(m1b, HIGH);
+      digitalWrite(m2a, LOW);
+      digitalWrite(m2b, HIGH); 
+    }
+  
+    else if(val == 'L') //Left
+    {
+    digitalWrite(m1a, HIGH);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, LOW);
+    digitalWrite(m2b, HIGH);
+    }
+    else if(val == 'R') //Right
+    {
+    digitalWrite(m1a, LOW);
+    digitalWrite(m1b, HIGH);
+    digitalWrite(m2a, HIGH);
+    digitalWrite(m2b, LOW); 
+    }
+    
+    else if(val == 'S') //Stop
+    {
+    digitalWrite(m1a, LOW);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, LOW);
+    digitalWrite(m2b, LOW); 
+    }
+    else if(val == 'I') //Forward Right
+    {
+    digitalWrite(m1a, LOW);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, HIGH);
+    digitalWrite(m2b, LOW);
+    }
+    else if(val == 'J') //Backward Right
+    {
+    digitalWrite(m1a, LOW);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, LOW);
+    digitalWrite(m2b, HIGH);
+    }
+    else if(val == 'G') //Forward Left
+    {
+    digitalWrite(m1a, HIGH);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, LOW);     
+    digitalWrite(m2b, LOW);
+    }
+    else if(val == 'H') //Backward Left
+    {
+    digitalWrite(m1a, LOW);
+    digitalWrite(m1b, HIGH);
+    digitalWrite(m2a, LOW);
+    digitalWrite(m2b, LOW); 
+    }
+    else if (val == 'V')
+    {
+      digitalWrite(horn, HIGH);
+      delay(1000);
+    }
+    else if (val == 'v')
+    {
+    digitalWrite(horn, LOW);
+    delay(1000);
+    }
+    
+}
